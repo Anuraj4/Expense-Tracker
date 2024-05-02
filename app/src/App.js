ab40281933d4ec30f242f286cefe351dc3d496ee
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-05-02 16:10:59 +0530

    adding CSS

--- app/src/App.js
+++ app/src/App.js
@@ -21,32 +21,32 @@ function App() {
             <div className = "description">It was time for new tv</div>
           </div>
           <div className="right">
-            <div className = "price" >
-              $500
+            <div className = "price red" >
+              -$600
             </div>
             <div className = "datetime">2022-12-18 15:45</div>
           </div>
         </div>
         <div className="transaction">
           <div className="left">
-            <div className = "name">New Samsung TV</div>
+            <div className = "name">New Onida TV</div>
             <div className = "description">It was time for new tv</div>
           </div>
           <div className="right">
-            <div className = "price" >
-              $500
+            <div className = "price green" >
+              +$500
             </div>
             <div className = "datetime">2022-12-18 15:45</div>
           </div>
         </div>
         <div className="transaction">
           <div className="left">
-            <div className = "name">New Samsung TV</div>
+            <div className = "name">New LG TV</div>
             <div className = "description">It was time for new tv</div>
           </div>
           <div className="right">
-            <div className = "price" >
-              $500
+            <div className = "price red" >
+              -$400
             </div>
             <div className = "datetime">2022-12-18 15:45</div>
           </div>
